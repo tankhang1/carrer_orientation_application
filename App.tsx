151a6fbcf17d7aeebfb3ea4c44e998ed9f1a6a5b
Tien25012003 <21520112@gm.uit.edu.vn>
2024-03-15 08:59:04 +0700

    Feat - Config typescript

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {atom, useAtom} from 'jotai';
 import {atomWithQuery} from 'jotai-tanstack-query';
 import {useFlipper} from '@react-navigation/devtools';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import {COLORS} from '@utils/config';
 const idAtom = atom(1);
 const userAtom = atomWithQuery(get => ({
   queryKey: ['users', get(idAtom)],
@@ -18,7 +19,7 @@ const App = () => {
   if (isError) return <Text>Error</Text>;
   return (
     <View>
-      <Text style={{fontFamily: 'Raleway-Bold'}}>
+      <Text style={{fontFamily: 'Raleway-Bold', color: COLORS.orange}}>
         {JSON.stringify(data, null, 2)}
       </Text>
       <AntDesign name="arrowright" />
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,31 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src/'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@service': './src/service',
+          '@store': './src/store',
+          '@utils': './src/utils',
+        },
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+      },
+    ],
+    ['react-native-reanimated/plugin'],
+  ],
 };
