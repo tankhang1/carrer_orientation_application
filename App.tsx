812edf8aa69f71b050f12fadf573b5d7aebdc688
Tien25012003 <21520112@gm.uit.edu.vn>
2024-03-16 13:13:52 +0700

    Feat - Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {atom, useAtom} from 'jotai';
 import {atomWithQuery} from 'jotai-tanstack-query';
 import {useFlipper} from '@react-navigation/devtools';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {COLORS} from '@utils/config';
 import AppNavigation from '@navigation';
+import BootSplash from 'react-native-bootsplash';
 
 const App = () => {
+  useEffect(() => {
+    BootSplash.hide({fade: true});
+  }, []);
   return <AppNavigation />;
 };
 
