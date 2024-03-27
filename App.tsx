27a5c728ca3308b450cd9428d279a97a78ae9b66
Tien25012003 <21520112@gm.uit.edu.vn>
2024-03-28 00:58:38 +0700

    Fix bug - Remove Flipper

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
 import {atom, useAtom} from 'jotai';
 import {atomWithQuery} from 'jotai-tanstack-query';
-import {useFlipper} from '@react-navigation/devtools';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {COLORS} from '@utils/config';
 import AppNavigation from '@navigation';
